f8978eada7791e38ed17b18cd05cd2fc44b266ce
Martin Donadieu <martindonadieu@gmail.com>
2023-08-05 01:23:20 +0100

    fix: clear cookie only on current url

--- android/src/main/java/ee/forgr/capacitor_inappbrowser/InAppBrowserPlugin.java
+++ android/src/main/java/ee/forgr/capacitor_inappbrowser/InAppBrowserPlugin.java
@@ -28,6 +28,7 @@ public class InAppBrowserPlugin extends Plugin {
   private CustomTabsClient customTabsClient;
   private CustomTabsSession currentSession;
   private WebViewDialog webViewDialog = null;
+  private String currentUrl = "";
 
   CustomTabsServiceConnection connection = new CustomTabsServiceConnection() {
     @Override
@@ -49,6 +50,7 @@ public class InAppBrowserPlugin extends Plugin {
     if (url == null || TextUtils.isEmpty(url)) {
       call.reject("Invalid URL");
     }
+    currentUrl = url;
     this.getActivity()
       .runOnUiThread(
         new Runnable() {
@@ -71,6 +73,7 @@ public class InAppBrowserPlugin extends Plugin {
     if (url == null || TextUtils.isEmpty(url)) {
       call.reject("Invalid URL");
     }
+    currentUrl = url;
     CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(
       getCustomTabsSession()
     );
@@ -110,11 +113,29 @@ public class InAppBrowserPlugin extends Plugin {
     call.resolve();
   }
 
-  @PluginMethod
-  public void clearCookies(PluginCall call) {
-    WebStorage.getInstance().deleteAllData();
-    call.resolve();
-  }
+@PluginMethod
+public void clearCookies(PluginCall call) {
+    if (webViewDialog == null) {
+        call.reject("WebView is not open");
+    } else {
+        String url = currentUrl;
+        if (url == null || TextUtils.isEmpty(url)) {
+            call.reject("Invalid URL");
+        } else {
+            CookieManager cookieManager = CookieManager.getInstance();
+            String cookie = cookieManager.getCookie(url);
+            if (cookie != null) {
+                String[] cookies = cookie.split(";");
+                for (String c : cookies) {
+                    String cookieName = c.substring(0, c.indexOf("="));
+                    cookieManager.setCookie(url, cookieName + "=; Expires=Thu, 01 Jan 1970 00:00:01 GMT");
+                }
+            }
+            call.resolve();
+        }
+    }
+}
+
 
   @PluginMethod
   public void openWebView(PluginCall call) {
@@ -122,6 +143,7 @@ public class InAppBrowserPlugin extends Plugin {
     if (url == null || TextUtils.isEmpty(url)) {
       call.reject("Invalid URL");
     }
+    currentUrl = url;
     final Options options = new Options();
     options.setUrl(url);
     options.setHeaders(call.getObject("headers"));
