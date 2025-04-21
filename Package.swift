7e49bac0f0e374bbf3742559fdb79bbf55e524b7
Martin Donadieu <martindonadieu@gmail.com>
2025-04-21 14:14:41 +0100

    feat: add SPM

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapgoCapacitorInappbrowser",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "CapgoCapacitorInappbrowser",
+            targets: ["InappbrowserPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "InappbrowserPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+            ],
+            path: "ios/Sources/InappbrowserPlugin"),
+        .testTarget(
+            name: "InappbrowserPluginTests",
+            dependencies: ["InappbrowserPlugin"],
+            path: "ios/Tests/InappbrowserPluginTests")
+    ]
+)
