112b5dde18fa89f4c223a29c6db9c235b429a528
Eljas <eljas.jalo@fiksuruoka.fi>
2025-09-22 14:48:17 +0300

    chore: ensure npm run verify:ios succeeded

--- Package.swift
+++ Package.swift
@@ -23,6 +23,6 @@ let package = Package(
         .testTarget(
             name: "InappbrowserPluginTests",
             dependencies: ["InappbrowserPlugin"],
-            path: "ios/PluginTests/InappbrowserPluginTests")
+            path: "ios/PluginTests")
     ]
 )
