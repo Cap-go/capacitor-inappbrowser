23f02e4d5f023692b87acc6eb4d1706707632aed
Alberto Monterroso <14013679+Albermonte@users.noreply.github.com>
2026-02-16 10:35:55 -0500

    fix(android): allow show() with AWARE invisibility mode (#442)

--- android/src/main/java/ee/forgr/capacitor_inappbrowser/WebViewDialog.java
+++ android/src/main/java/ee/forgr/capacitor_inappbrowser/WebViewDialog.java
@@ -1080,10 +1080,6 @@ public class WebViewDialog extends Dialog {
         return isHiddenModeActive;
     }
 
-    public boolean isFakeVisibleMode() {
-        return _options != null && _options.getInvisibilityMode() == Options.InvisibilityMode.FAKE_VISIBLE;
-    }
-
     /**
      * Apply window insets to the WebView to properly handle edge-to-edge display
      * and fix status bar overlap issues on Android 15+
