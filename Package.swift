890570b4c07d3ffc150656ed0f198e9f481a1a85
WcaleNieWolny <isupermichael007@gmail.com>
2026-03-25 06:56:08 +0100

    feat(ios): add SwiftNIO and SwiftNIO-SSL dependencies

--- Package.swift
+++ Package.swift
@@ -10,14 +10,21 @@ let package = Package(
             targets: ["InappbrowserPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.27.0"),
     ],
     targets: [
         .target(
             name: "InappbrowserPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
             ],
             path: "ios/Sources/InAppBrowserPlugin"),
         .testTarget(
